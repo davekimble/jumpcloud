ac2581788b27e157180d39b826d3876d9c2cd9e2
davekimble <dave@beneaththeink.com>
2016-04-06 14:49:16 -0600

    added script file to test multiple simultaneous connections

---
+++ curlScript.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+curl http://localhost:8080?password=angryMonkey&
+curl http://localhost:8080?password=newOne&
+curl http://localhost:8080?password=funny&
+curl http://localhost:8080?password=notSoFunny&
