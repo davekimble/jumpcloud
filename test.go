9bd542060b9c1df6eaf752a64cb57ceae46d4d0c
davekimble <dave@beneaththeink.com>
2016-04-06 12:17:34 -0600

    added simple http server

--- test.go
+++ test.go
@@ -5,11 +5,19 @@ import (
     "crypto/sha512"
     "io"
     "encoding/base64"
+    "net/http"
 )
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
 
 func main() {
     var pw = "angryMonkey"
     hash := sha512.New()
     io.WriteString( hash, pw )
     fmt.Println(base64.URLEncoding.EncodeToString(hash.Sum(nil)))
+
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
